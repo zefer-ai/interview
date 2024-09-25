bcb3a99c2ed1235e38958812a21bb7afa2113587
Armin Ghobadi <arminghobadi@Armins-MacBook-Pro.local>
2024-09-25 14:32:22 -0400

    database config is added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import { initializeApp } from "firebase/app";
 import { AppRoutes } from './AppRoutes';
 
 const firebaseConfig = {
-  // FILL ME
+  apiKey: "AIzaSyDhGza0BUIHAYPlkY6YrLx4KSkVF54XYC4",
+  authDomain: "zefer-interview.firebaseapp.com",
+  projectId: "zefer-interview",
+  storageBucket: "zefer-interview.appspot.com",
+  messagingSenderId: "681140573240",
+  appId: "1:681140573240:web:9c82f70cdc2742d088c2cd",
+  measurementId: "G-WYJ3Z23V6T"
 };
 
 // Initialize Firebase
